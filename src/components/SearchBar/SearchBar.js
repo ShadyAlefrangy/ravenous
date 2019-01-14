79df67ebaaf38a363e2bd5b9b650bc6c01ed1c92
Shady Alefrangy <shadyahmed38@gmail.com>
2019-01-14 19:41:28 +0200

    Simulated a search query with the Let's Go button

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -18,6 +18,7 @@ class SearchBar extends React.Component {
         };
         this.handleTermChange = this.handleTermChange.bind(this);
         this.handleLocationChange = this.handleLocationChange.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
     }
 
     // dynamically create the list items needed to display the sort options
@@ -49,6 +50,11 @@ class SearchBar extends React.Component {
         this.setState({location: event.target.value});
     };
 
+    handleSearch(event) {
+        this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+        event.preventDefault();
+    };
+
     render() {
         return (
             <div className="SearchBar">
@@ -62,7 +68,7 @@ class SearchBar extends React.Component {
                     <input placeholder="Where?" onChange={this.handleLocationChange} />
                 </div>
                 <div className="SearchBar-submit">
-                    <a>Let's Go</a>
+                    <a onClick={this.handleSearch}>Let's Go</a>
                 </div>
             </div>
         );
